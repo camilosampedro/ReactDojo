af41e056a2c43ed9d8b3e431b59e72cced41e046
MarcelaSantos22 <ymarcela.quejada@udea.edu.co>
2017-03-01 22:51:55 -0500

    Cambio de vista

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Categorias from './Categorias/Categorias'
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Categorias />,
   document.getElementById('root')
 );
